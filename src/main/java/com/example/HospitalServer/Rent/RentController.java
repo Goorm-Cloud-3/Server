68e735e9101721de3bfae727651a34a5456261bd
chantro <sophia2359@gmail.com>
2025-03-13 16:41:48 +0900

    RentController -> 출생연도 정보 추가

--- src/main/java/com/example/HospitalServer/Rent/RentController.java
+++ src/main/java/com/example/HospitalServer/Rent/RentController.java
@@ -6,15 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/test")
 public class RentController {
     private final String default_text = "hello, ";
 
     @GetMapping("/hello")
-    public ResponseEntity<?> ReturnHello(@RequestParam("name") String name){
-        String text = default_text + name;
+    public ResponseEntity<?> ReturnHello(@RequestParam("name") String name,
+                                         @RequestParam("age") int age){
+
+        String birth_year = "" + CalAge(age);
+        String text = default_text + name + "(" + birth_year + ")";
 
         return ResponseEntity.ok(text);
+    }
+
+    private int CalAge(int age){
+        return LocalDate.now().getYear() - (age-1);
     }
 }
